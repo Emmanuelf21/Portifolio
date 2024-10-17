3406dd54e066c43fbcdcc2498c148232e7114d2d
Emmanuel Franco <emmanuelfranco2001@gmail.com>
2024-10-17 14:53:33 -0300

    finalizado o formulário

--- php/enviaEmail.php
+++ php/enviaEmail.php
@@ -1,3 +1,22 @@
 <?php
 
-?>
+    $nome = addslashes($_POST['nome']);
+    $email = addslashes($_POST['email']);
+    $telefone = addslashes($_POST['telefone']);
+    $razao = addslashes($_POST['razao']);
+    $mensagem = addslashes($_POST['mensagem']);
+
+    $para = "emmanuelfranco2001@gmail.com";
+    $assunto = "Contato Portifólio";
+
+    $corpo = "Nome: ".$nome."\n"."E-mail: ".$email."\n"."Telefone para contato: ".$telefone."\n"."Motivo: ".$razao."\n"."Mensagem: ".$mensagem;
+
+    $cabeca = "From: emmanuelfranco2001@gmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
+
+    if(mail($para,$assunto,$corpo,$cabeca)){
+        echo("E-mail enviado com sucesso!");
+    }else{
+        echo("Houve um erro ao enviar o email!");
+    }
+    
+ 
